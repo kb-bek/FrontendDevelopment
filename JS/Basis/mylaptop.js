fbbb06b10881b459cdc18e37beec2b9f69e4d895
Kubanychbek Talipov <talipovkuba555@gmail.com>
2024-01-03 13:31:29 +0600

    Add js files

---
+++ JS/Basis/mylaptop.js
@@ -0,0 +1,18 @@
+/*
+Опишите с помощью переменных свой ноутбук.
+*/
+
+const cpu = 'core i5 8th gen';
+const monitor = '15.6 HD';
+const hdd = 1;
+const ssd = 128;
+const ram = 8;
+const gpu = 'Nvidia Geforce mx150 4gb';
+
+console.log(`Характеристики ноутбука:
+Процессор: ${cpu}
+Монитор: ${monitor}
+HDD: ${hdd} Tb
+SSD: ${ssd} Gb
+RAM: ${ram} Gb
+GPU: ${gpu}`)
